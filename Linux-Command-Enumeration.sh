93e59e4cd2fec1a48b8b5f954e97e84f76d54c15
K0Hack <82372292+Ki-pr0@users.noreply.github.com>
2021-06-09 18:40:11 +0200

    Create Linux-Command-Enumeration.sh

---
+++ Linux-Command-Enumeration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Comando para enumerar desde la ruta /var/ en busca de credenciales para escalar Privilegios
+
+$ find . -iname '*config*' -type f -exec grep -nie 'pass.*=' --color=always /dev/null {} \;
+
+# Buscando por permisos SUID a nivel de root para escalar privilegios
+
+$ find / -perm -u=s -type f 2>/dev/null
+
+# Busqueda de archivos interesantes con el nombre +config+
+
+$ find \-name config* -type f 2>/dev/null
+ 
+$ find \-name *config* -type f 2>/dev/null
+
+# Grepeando desde la ruta /var/www/ para encontrar ficheros de manera recursiva que contengan el multiple matching de estas palabras
+
+/var/www $ grep -r -i -E "user|pass|auth|key|db|database"
+
+# Variaciones para reducir los contenidos listados y poder asimilarlos al verlos o buscar informacion
+
+$ grep -r -i -E "user|pass"
+$ grep -r -i -E "auth|key|db|database"
