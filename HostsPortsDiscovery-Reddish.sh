8d0ca7a257e69530261dc6868775f63af16921b6
K0Hack <82372292+Ki-pr0@users.noreply.github.com>
2021-06-15 10:30:16 +0200

    Create HostsPortsDiscovery-Reddish.sh

---
+++ HostsPortsDiscovery-Reddish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Descubrimiento de Puertos para los Siguientes Hosts encontrados en la Maquina Reddish
+hosts=("172.18.0.1" "172.19.0.1" "172.19.0.2" "172.19.0.3")
+
+for host in ${hosts[@]}; do
+        echo -e "\n[+]Scanning ports in $hosts \n"
+        for port in $(seq 1 10000); do
+                timeout 1 bash -c "echo '' > /dev/tcp/$host/$port" 2> /dev/null && echo -e "\t[*] Puerto: $port - ABIERTO [*]" &
+        done; wait
+done
