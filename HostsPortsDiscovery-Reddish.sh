2be9a9c00e311c338adbf9a84587cc247caeff11
K0Hack <82372292+Ki-pr0@users.noreply.github.com>
2021-06-15 11:22:06 +0200

    Update HostsPortsDiscovery-Reddish.sh

--- HostsPortsDiscovery-Reddish.sh
+++ HostsPortsDiscovery-Reddish.sh
@@ -3,7 +3,7 @@
 hosts=("172.18.0.1" "172.19.0.1" "172.19.0.2" "172.19.0.3")
 
 for host in ${hosts[@]}; do
-        echo -e "\n[+]Scanning ports in $hosts \n"
+        echo -e "\n[+]Scanning ports in $host \n"
         for port in $(seq 1 10000); do
                 timeout 1 bash -c "echo '' > /dev/tcp/$host/$port" 2> /dev/null && echo -e "\t[*] Puerto: $port - ABIERTO [*]" &
         done; wait
